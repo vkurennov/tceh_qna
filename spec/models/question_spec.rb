7c23eec744772c40c0897b85a6085c96d571282e
Vitaly Kurennov <vkurennov@gmail.com>
2015-12-10 20:20:23 +0300

    Question model with specs

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  # it 'validates presence of title' do
+  #   question = Question.new(body: '123')
+  #   expect(question).not_to be_valid
+  # end
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+
+end
